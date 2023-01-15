879587a0bc88c02c45b9bf68332ce0e9a8bb7344
MatheusBrandao525 <mafe123silva@gmail.com>
2023-01-15 12:25:38 -0300

    LifeClycle Hooks

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <PrimeiroComponente/>
   <p>O estado da aplicação é: {{ testando }}</p>
+  <LifeCycle/>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
-import LifeCycle from './ '
+import LifeCycle from './components/LifeCycle.vue'
 
   export default {
       name: 'App',
       components: {
-        PrimeiroComponente
+        PrimeiroComponente,
+        LifeCycle
       },
       data() {
         return {
--- projeto_vue/src/components/LifeCycle.vue
+++ projeto_vue/src/components/LifeCycle.vue
@@ -10,6 +10,18 @@
             return {
                 nome: 'Ainda não sei'
             }
-        }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Matheus'
+
+            }, 1000);
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'Pedro'
+
+            }, 2000);
+        },
     }
 </script>
