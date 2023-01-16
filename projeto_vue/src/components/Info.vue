849c4672e2f1363d0bda2d3dde1be6102126256f
MatheusBrandao525 <mafe123silva@gmail.com>
2023-01-16 11:19:01 -0300

    Diretivas v-if, v-else e v-show

--- projeto_vue/src/components/Info.vue
+++ projeto_vue/src/components/Info.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <p>Estou buscando uma oportunidade.</p>
+        <p v-if="esta_trabalhando">Estou Trabalhando.</p>
+        <p v-else>Estou buscando uma oportunidade.</p>
+
         <p>utilizo as seguintes tecnologias:</p>
         <ul>
             <li>HTML</li>
@@ -15,11 +17,20 @@
             <li>Vue.js</li>
             <li>Node.js</li>
         </ul>
+
+        <p v-show="mostrar_email">Mande uma menssagem para : {{ email }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                esta_trabalhando: false,
+                mostrar_email: true,
+                email: 'brandao.matheus.dev@gmail.com'
+            }
+        }
     }
 </script>
